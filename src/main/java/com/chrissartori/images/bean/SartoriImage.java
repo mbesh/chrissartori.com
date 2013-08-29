39aa236b8af1a1f321585762513691904d6aa4f4
mbesh <mbesh@JohnnyFresh>
2013-08-28 23:07:13 -0400

    Adding Spring initial configuration, controller, and generic bean.

---
+++ src/main/java/com/chrissartori/images/bean/SartoriImage.java
@@ -0,0 +1,24 @@
+package com.chrissartori.images.bean;
+
+public class SartoriImage {
+
+	private String url;
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
